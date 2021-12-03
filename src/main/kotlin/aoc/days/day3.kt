621762b60330a6163e78b5e66de7dd4389c4c668
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-12-04 00:23:07 +0100

    day3: simplify implementation

--- src/main/kotlin/aoc/days/day3.kt
+++ src/main/kotlin/aoc/days/day3.kt
@@ -1,77 +1,52 @@
 package aoc.days
 
 import aoc.inputOfDay
-import aoc.days.Bit.One
-import aoc.days.Bit.Zero
-import kotlin.math.pow
-
-enum class Bit {
-    Zero, One;
-
-    fun toInt(bitPosition: Int) = when (this) {
-        Zero -> 0
-        One -> (2.toDouble().pow(bitPosition - 1).toInt())
-    }
-
-    companion object {
-        fun of(char: Char): Bit = when (char) {
-            '0' -> Zero
-            '1' -> One
-            else -> error("invalid bit")
-        }
-    }
-}
-typealias BinaryNum = List<Bit>
-
-fun BinaryNum.toInt() = this.mapIndexed { i, bit -> bit.toInt(i + (this.size - i * 2)) }.sum()
 
 fun main() {
-    val input = inputOfDay(3).map { bin -> bin.map { Bit.of(it) } }
+    val input = inputOfDay(3)
     println(task1(input))
     println(task2(input))
 }
 
-fun task1(input: List<BinaryNum>): Int {
-    val bits = input[0].size
-
-    tailrec fun task1Recursively(input: List<BinaryNum>, index: Int, current: Pair<Int, Int>): Pair<Int, Int> {
-        if (index >= input[0].size) return current
-
-        val oneCount = input.count { it[index] == One }
-        val decimal = One.toInt(index + (bits - index * 2))
-        val updated =
-            if (oneCount > (input.size / 2)) Pair(current.first + decimal, current.second)
-            else Pair(current.first, current.second + decimal)
-
-        return task1Recursively(input, index, updated)
+fun task1(input: List<String>): Int {
+    tailrec fun findMostCommonBits(
+        input: List<String>,
+        index: Int,
+        currentBinary: String,
+        appendOne: (oneCount: Int) -> Boolean
+    ): String = when {
+        index >= input[0].length -> currentBinary
+        else -> {
+            val oneCount = input.count { it[index] == '1' }
+            val updatedString = currentBinary + (if (appendOne(oneCount)) '1' else '0')
+            findMostCommonBits(input, index + 1, updatedString, appendOne)
+        }
     }
 
-    return task1Recursively(input, 0, Pair(0, 0)).let { it.first * it.second }
+    val elementsHalf = input.size / 2
+    val gamma = findMostCommonBits(input, 0, "") { it > elementsHalf }
+    val epsilon = findMostCommonBits(input, 0, "") { it < elementsHalf }
+    return gamma.toInt(2) * epsilon.toInt(2)
 }
 
-fun task2(input: List<BinaryNum>): Int {
+fun task2(input: List<String>): Int {
     tailrec fun findSingleRecursive(
-        input: List<BinaryNum>,
+        input: List<String>,
         index: Int,
-        equalityBreaker: Bit,
-        chooseLarger: Boolean
-    ): BinaryNum {
-        val split = input.partition { it[index] == One }
+        chooseLarger: Boolean,
+        tieBreaker: (ones: List<String>, zeros: List<String>) -> List<String>
+    ): String {
+        val (ones, zeros) = input.partition { it[index] == '1' }
         val filtered = when {
-            split.first.size == split.second.size ->
-                when (equalityBreaker) {
-                    One -> split.first
-                    Zero -> split.second
-                }
-            split.first.size > split.second.size && chooseLarger -> split.first
-            split.first.size < split.second.size && !chooseLarger -> split.first
-            else -> split.second
+            ones.size == zeros.size -> tieBreaker(ones, zeros)
+            ones.size > zeros.size && chooseLarger -> ones
+            ones.size < zeros.size && !chooseLarger -> ones
+            else -> zeros
         }
-        return filtered.singleOrNull() ?: findSingleRecursive(filtered, index + 1, equalityBreaker, chooseLarger)
+        return filtered.singleOrNull() ?: findSingleRecursive(filtered, index + 1, chooseLarger, tieBreaker)
     }
 
-    val ogr = findSingleRecursive(input, 0, One, true)
-    val ocr = findSingleRecursive(input, 0, Zero, false)
-
-    return ogr.toInt() * ocr.toInt()
+    val o2 = findSingleRecursive(input, 0, true) { ones, _ -> ones }
+    val co2 = findSingleRecursive(input, 0, false) { _, zeros -> zeros }
+    return o2.toInt(2) * co2.toInt(2)
 }
