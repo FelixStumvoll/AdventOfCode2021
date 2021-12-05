12c2d462534885b451818cc50836bdf235f902f2
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-12-05 12:18:41 +0100

    day5: solve

---
+++ src/main/kotlin/aoc/days/day5/day5.kt
@@ -0,0 +1,40 @@
+package aoc.days.day5
+
+import aoc.inputOfDay
+
+data class Point(val x: Int, val y: Int) {
+    infix fun to(other: Point): List<Point> {
+        val xRange = if(x >= other.x)  x.downTo(other.x) else x..other.x
+        val yRange = if(y >= other.y) y.downTo(other.y) else y..other.y
+
+        return when {
+            x == other.x -> yRange.map { Point(x, it) }
+            y == other.y -> xRange.map { Point(it, y) }
+            else -> xRange.zip(yRange).map { (x,y) -> Point(x, y) }
+        }
+    }
+}
+
+typealias VentLines = List<Pair<Point, Point>>
+
+fun parseInput(input: List<String>): VentLines = input
+    .map { line ->
+        line.split(" -> ")
+            .map { pair -> pair.split(",").let { (x, y) -> Point(x.toInt(), y.toInt()) } }
+            .let { (start, end) -> Pair(start, end) }
+    }
+
+fun main() {
+    val input = parseInput(inputOfDay(5))
+    println(task1(input))
+    println(task2(input))
+}
+
+fun buildVentMap(input: VentLines, filter: ((start: Point, end: Point) -> Boolean)? = null): Int =
+    input.filter { (start, end) -> filter?.invoke(start, end) ?: true }
+        .flatMap { (start, end) -> start to end }
+        .groupBy { it.x }.mapValues { (_, v) -> v.groupingBy { it.y }.eachCount() }
+        .map { it.value.filter { (_, v) -> v > 1 }.values.count() }.sum()
+
+fun task1(input: VentLines): Int = buildVentMap(input) { start, end -> start.x == end.x || start.y == end.y }
+fun task2(input: VentLines): Int = buildVentMap(input)
