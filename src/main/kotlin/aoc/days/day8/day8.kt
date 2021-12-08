b2e23e4147a8eaa1da109adb9a6d60b268841bed
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-12-08 15:14:03 +0100

    day8: solve task2

--- src/main/kotlin/aoc/days/day8/day8.kt
+++ src/main/kotlin/aoc/days/day8/day8.kt
@@ -7,6 +7,7 @@ typealias PatternEntry = Pair<List<String>, List<String>>
 fun main() {
     val input = parseInput(inputOfDay(8))
     println(task1(input))
+    println(task2(input))
 }
 
 fun parseInput(input: List<String>): List<PatternEntry> {
@@ -27,3 +28,27 @@ fun task1(input: List<PatternEntry>): Int = input
             } as Int
         }
     }
+
+fun task2(input: List<PatternEntry>): Int {
+    infix fun String.allIn(other: String) = this.all { it in other }
+    fun List<String>.firstWithLength(length: Int): String = this.first { it.length == length }
+    fun getNumber(number: String, pattern: List<String>): Char = when (number.length) {
+        2 -> '1'
+        4 -> '4'
+        3 -> '7'
+        7 -> '8'
+        5 -> {
+            val one by lazy { pattern.firstWithLength(2) }
+            val six by lazy { pattern.filter { it.length == 6 }.first { getNumber(it, pattern) == '6' } }
+            if (one allIn number) '3' else if (number allIn six) '5' else '2'
+        }
+        6 -> {
+            val four by lazy { pattern.firstWithLength(4) }
+            val one by lazy { pattern.firstWithLength(2) }
+            if (four allIn number) '9' else if (one allIn number) '0' else '6'
+        }
+        else -> error("invalid pattern length")
+    }
+
+    return input.sumOf { (pattern, numbers) -> numbers.map { getNumber(it, pattern) }.joinToString("").toInt() }
+}
